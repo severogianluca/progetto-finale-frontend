9b609b1a99d60509747853c00701c52ac71c42a1
severogianluca <gianlucasevero300@gmail.com>
2025-09-05 02:19:36 +0200

    Feat: add HomePage

--- pages/HomeSbeam.jsx
+++ pages/HomeSbeam.jsx
@@ -1,11 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import VideogameCard from "../component/VideogameCard";
 
 
 function HomeSbeam() {
+
+    const [videogames, setVideogames] = useState([])
+
+    function fetchVideogames() {
+
+        axios
+            .get(import.meta.env.VITE_API_URL)
+            .then((response) => setVideogames(response.data.data))
+            .catch((err) => console.error(err))
+
+    }
+
+    useEffect(fetchVideogames,[])
 
     return (
 
         <>
             <h2>Benvenuti su Sbeam</h2>
+            {
+                videogames.map((videogame) =>
+                <VideogameCard key={videogame.id} videogame={videogame}/>)
+            }
         </>
     )
 }
