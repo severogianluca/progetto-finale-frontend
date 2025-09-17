7051eb134077002e6aa1022f19dda0c4b79ce32f
severogianluca <gianlucasevero300@gmail.com>
2025-09-17 05:01:33 +0200

    Feat: add loader

--- pages/HomeSbeam.jsx
+++ pages/HomeSbeam.jsx
@@ -2,12 +2,13 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import VideogameCard from "../component/VideogameCard";
 import LoaderContext from "../context/LoaderContext";
+import Loader from "../component/Loader";
 
 
 function HomeSbeam() {
 
     const [videogames, setVideogames] = useState([])
-    const { setIsLoading } = useContext(LoaderContext)
+    const { isLoading, setIsLoading } = useContext(LoaderContext)
 
 
     function fetchVideogames() {
@@ -16,7 +17,8 @@ function HomeSbeam() {
         axios
             .get(import.meta.env.VITE_API_URL)
             .then((response) => {
-                setVideogames(response.data.data)} )
+                setVideogames(response.data.data)
+            })
             .catch((err) => console.error(err))
             .finally(() => setIsLoading(false))
 
@@ -28,15 +30,22 @@ function HomeSbeam() {
     return (
 
         <>
-            <img src="/public/metal-gear-header.png" alt="metal-gear-jumbo" className="img-fluid w-100" />
-            <div className="container">
-                <h2 className="mt-5 mb-4">Benvenuti su Sbeam</h2>
-                {
-                    videogames.map((videogame) =>
-                        <VideogameCard key={videogame.id} videogame={videogame} />)
-                }
-            </div>
-
+            {isLoading ? (
+                <Loader />
+            ) : (
+                videogames.length > 0 && (
+                    <>
+                        <img src="/public/metal-gear-header.png" alt="metal-gear-jumbo" className="img-fluid w-100" />
+                        <div className="container">
+                            <h2 className="mt-5 mb-4">Benvenuti su Sbeam</h2>
+                            {
+                                videogames.map((videogame) =>
+                                    <VideogameCard key={videogame.id} videogame={videogame} />)
+                            }
+                        </div>
+                    </>
+                )
+            )}
         </>
     )
 }
